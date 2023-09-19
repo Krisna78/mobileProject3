892b3298862a2985f9c949bbe686dc2af975f517
E41220709_Yohanes Krisna Desantho <desanthokrisna78@gmail.com>
2023-09-19 13:09:58 +0700

    add mainActivity and direct button login to main

--- app/src/main/java/com/example/listview/loginActivity.java
+++ app/src/main/java/com/example/listview/loginActivity.java
@@ -2,13 +2,29 @@ package com.example.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginActivity extends AppCompatActivity {
-
+    private EditText loginUsername, loginPassword;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginPassword = findViewById(R.id.password_field);
+        loginUsername = findViewById(R.id.username_field);
+        loginButton = findViewById(R.id.btn_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginActivity.this,MainActivity.class));
+            }
+        });
     }
 }
